99682c92b31948d31a2ab808f3a5f4ac6867e869
servando coria <servando.coria@sonatasmx.com>
2018-09-04 15:53:12 -0700

    Ejemplo basico de Manejo de excepciones con finally

---
+++ ExamRef70483.Obj1_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRef70483.Obj1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            try
+            {
+                int i = int.Parse(s);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You need to enter a value");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not a valid number. Please try again", s);
+            }
+            finally
+            {
+                Console.WriteLine("Program complete.");
+            }
+        }
+    }
+}

3023548cd3cbc593fc4c183260762466988018ee
servando coria <servando.coria@sonatasmx.com>
2018-09-04 11:51:04 -0700

    Ejemplo simple de delegates

---
+++ ExamRef70483.Obj1_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRef70483.Obj1_4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            UnderstangingDelegates understangingDelegates = new UnderstangingDelegates();
+
+            understangingDelegates.UseDelegate();
+
+            Console.Read();
+        }
+    }
+}
---
+++ ExamRef70483.Obj1_4/UnderstangingDelegates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRef70483.Obj1_4
+{
+    public class UnderstangingDelegates
+    {
+        public delegate int Calculate(int x, int y);
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public void UseDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4));
+        }
+    }
+}

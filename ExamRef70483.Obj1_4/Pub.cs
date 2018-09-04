6bd25221719c0dd3601d8b3e3afa55902fd62720
servando coria <servando.coria@sonatasmx.com>
2018-09-04 15:18:42 -0700

    Custom Event Accessor

--- ExamRef70483.Obj1_4/Pub.cs
+++ ExamRef70483.Obj1_4/Pub.cs
@@ -8,16 +8,33 @@ namespace ExamRef70483.Obj1_4
 {
     public class Pub
     {
+        private event EventHandler<MisArgumentos> onChange = delegate { };
         /// <summary>
         /// un evento no puede ser asignado directamente (=) solo con (+=)
         /// Asi no corre riesgo de que alguien mas elimine los eventos anteriores
         /// otra cosa es que no puede ser ejecutado por usuarios externos si no por quien los define
         /// </summary>
-        public event EventHandler<MisArgumentos> OnChange = delegate { };
+        public event EventHandler<MisArgumentos> OnChange
+        {
+            add
+            {
+                lock (onChange)
+                {
+                    onChange += value;
+                }
+            }
+            remove
+            {
+                lock (onChange)
+                {
+                    onChange -= value;
+                }
+            }
+        }
         public void Raise()
         {
-            Console.WriteLine($"Invocation list: {OnChange.GetInvocationList().GetLength(0)}");
-            OnChange(this, new MisArgumentos(42));
+            Console.WriteLine($"Invocation list: {onChange.GetInvocationList().GetLength(0)}");
+            onChange(this, new MisArgumentos(42));
         }
     }
 

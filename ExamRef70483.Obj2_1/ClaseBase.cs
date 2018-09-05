2dc9ea491394340e80d0fc9973db2301b4640f6f
servando coria <servando.coria@sonatasmx.com>
2018-09-05 15:44:09 -0700

    virtual, override, sealed example

---
+++ ExamRef70483.Obj2_1/ClaseBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRef70483.Obj2_1
+{
+    class ClaseBase
+    {
+        /// <summary>
+        /// Le puedes hacer override por que es virtual
+        /// </summary>
+        /// <returns></returns>
+        public virtual int MyMethod_Overridable()
+        {
+            return 42;
+        }
+
+        public int MyMethod_NotVirtual()
+        {
+            return 42;
+        }
+    }
+
+    class ClaseDerivada : ClaseBase 
+    {
+        public override int MyMethod_Overridable()
+        {
+            return base.MyMethod_Overridable();
+        }
+
+        public int MyMethod_NotVirtual_From_ClaseDerivada()
+        {
+            return base.MyMethod_NotVirtual();
+        }
+    }
+}
